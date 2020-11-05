3c7340b4872b9ca5e930e9faea0ff313d10d60c4
midhun kp <mithula@adamscreation.com>
2020-11-05 11:06:38 +0530

    added pages

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,4 +1,8 @@
-$('.owl-carousel').owlCarousel({
+const prevIcon = '<img src="dist/images/left.svg"class="prev-btn">Prev';
+const nextIcon = 'Next<img src="dist/images/right.svg"class="next-btn">';
+
+
+$('.banner-slide').owlCarousel({
     loop: true,
     margin: 10,
     nav: false,
@@ -17,3 +21,88 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+$('.slide').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+
+        prevIcon, nextIcon
+    ],
+
+    pagination: false,
+    dots: true,
+    autoplay: 1000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+$('.company-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    pagination: false,
+    dots: true,
+    animateOut: 'fadeOut',
+    autoplay: 1000,
+    autoplaySpeed: 1000,
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+/**accordion */
+var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+    accordions[i].onclick = function() {
+        this.classList.toggle('is-open');
+
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            // accordion is currently open, so close it
+            content.style.maxHeight = null;
+        } else {
+            // accordion is currently closed, so open it
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+}
+
+/**tab */
+$(document).ready(function() {
+
+    $(".tabs-list li a").click(function(e) {
+        e.preventDefault();
+    });
+
+    $(".tabs-list li").click(function() {
+        var tabid = $(this).find("a").attr("href");
+        $(".tabs-list li,.tabs div.tab").removeClass("active"); // removing active class from tab
+        $(".tab").hide(); // hiding open tab
+        $(tabid).show(); // show tab
+        $(this).addClass("active"); //  adding active class to clicked tab
+
+
+    });
+
+});
